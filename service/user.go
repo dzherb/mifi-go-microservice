d130bc5fb398f2e901fb9e3a9c59e022f2dd7efa
Dmitrii Zherbin <zherbin.dima@yandex.ru>
2025-12-26 17:03:08 +0700

    feat: add user service and s3 storage logic

--- service/user.go
+++ service/user.go
@@ -1,1 +1,112 @@
 package service
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+	"time"
+
+	"github.com/google/uuid"
+
+	"github.com/dzherb/mifi-go-microservice/model"
+)
+
+type Storage[T any] interface {
+	Set(context.Context, string, T) error
+	Get(context.Context, string) (T, error)
+	Delete(context.Context, string) error
+}
+
+type User struct {
+	log     *slog.Logger
+	storage Storage[model.User]
+}
+
+func NewUser(log *slog.Logger, storage Storage[model.User]) *User {
+	return &User{
+		log:     log,
+		storage: storage,
+	}
+}
+
+var (
+	ErrMissingUserID = errors.New("missing user ID")
+)
+
+const userOperationsTimeout = 10 * time.Second
+
+func (u *User) Create(
+	ctx context.Context,
+	user model.User,
+) (model.User, error) {
+	ctx, cancel := context.WithTimeout(ctx, userOperationsTimeout)
+
+	defer cancel()
+
+	//if user.ID == "" {
+	//	return user, ErrMissingUserID
+	//}
+
+	createdUser := user
+	createdUser.ID = u.generateID()
+
+	err := u.storage.Set(ctx, u.buildStorageKey(user.ID), user)
+	if err != nil {
+		return user, fmt.Errorf("failed to create user: %w", err)
+	}
+
+	return createdUser, nil
+}
+
+func (u *User) Get(ctx context.Context, id string) (model.User, error) {
+	ctx, cancel := context.WithTimeout(ctx, userOperationsTimeout)
+
+	defer cancel()
+
+	user, err := u.storage.Get(ctx, u.buildStorageKey(id))
+	if err != nil {
+		return user, fmt.Errorf("failed to get user: %w", err)
+	}
+
+	return user, nil
+}
+
+func (u *User) Update(ctx context.Context, user model.User) error {
+	ctx, cancel := context.WithTimeout(ctx, userOperationsTimeout)
+
+	defer cancel()
+
+	if user.ID == "" {
+		return ErrMissingUserID
+	}
+
+	err := u.storage.Set(ctx, u.buildStorageKey(user.ID), user)
+
+	if err != nil {
+		return fmt.Errorf("failed to update user: %w", err)
+	}
+
+	return nil
+}
+
+func (u *User) Delete(ctx context.Context, id string) error {
+	ctx, cancel := context.WithTimeout(ctx, userOperationsTimeout)
+	defer cancel()
+
+	err := u.storage.Delete(ctx, u.buildStorageKey(id))
+
+	if err != nil {
+		return fmt.Errorf("failed to delete user: %w", err)
+	}
+
+	return nil
+}
+
+func (u *User) generateID() string {
+	return uuid.New().String()
+}
+
+func (u *User) buildStorageKey(userID string) string {
+	return fmt.Sprintf("user:%s", userID)
+}
---
+++ storage/minio.go
@@ -0,0 +1,141 @@
+package storage
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+)
+
+type MiniIO[T any] struct {
+	log        *slog.Logger
+	client     *minio.Client
+	bucketName string
+}
+
+type MiniIOConfig struct {
+	Endpoint   string
+	AccessKey  string
+	SecretKey  string
+	BucketName string
+	UseSSL     bool
+}
+
+func NewMiniIO[T any](
+	ctx context.Context,
+	log *slog.Logger,
+	cfg MiniIOConfig,
+) (*MiniIO[T], error) {
+	minioClient, err := minio.New(cfg.Endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(cfg.AccessKey, cfg.SecretKey, ""),
+		Secure: cfg.UseSSL,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to create MinIO client: %w", err)
+	}
+
+	storage := &MiniIO[T]{
+		log:        log,
+		client:     minioClient,
+		bucketName: cfg.BucketName,
+	}
+
+	// Создаем bucket если его нет
+	exists, err := minioClient.BucketExists(ctx, cfg.BucketName)
+	if err != nil {
+		return nil, fmt.Errorf("failed to check bucket existence: %w", err)
+	}
+
+	if !exists {
+		err = minioClient.MakeBucket(
+			ctx,
+			cfg.BucketName,
+			minio.MakeBucketOptions{},
+		)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create bucket: %w", err)
+		}
+
+		log.Info("bucket created", slog.String("bucket_name", cfg.BucketName))
+	}
+
+	return storage, nil
+}
+
+func (s *MiniIO[T]) Set(ctx context.Context, key string, data T) error {
+	dataSerialized, err := json.Marshal(data)
+	if err != nil {
+		return fmt.Errorf("marshaling data for key %s: %w", key, err)
+	}
+
+	reader := bytes.NewReader(dataSerialized)
+
+	_, err = s.client.PutObject(
+		ctx,
+		s.bucketName,
+		key,
+		reader,
+		int64(len(dataSerialized)),
+		minio.PutObjectOptions{
+			ContentType: "application/json",
+		},
+	)
+	if err != nil {
+		return fmt.Errorf("saving data to s3 with key %s: %w", key, err)
+	}
+
+	s.log.Info(
+		"data saved to s3",
+		slog.String("bucket_name", s.bucketName),
+		slog.String("key", key),
+	)
+
+	return nil
+}
+
+func (s *MiniIO[T]) Get(ctx context.Context, key string) (T, error) {
+	var res T
+
+	object, err := s.client.GetObject(
+		ctx,
+		s.bucketName,
+		key,
+		minio.GetObjectOptions{},
+	)
+	if err != nil {
+		return res, fmt.Errorf("failed to get object from s3: %w", err)
+	}
+
+	defer func() {
+		err := object.Close()
+		if err != nil {
+			s.log.Error("failed to close s3 object", slog.String("key", key))
+		}
+	}()
+
+	err = json.NewDecoder(object).Decode(&res)
+	if err != nil {
+		return res, fmt.Errorf("failed to unmarshal s3 object: %w", err)
+	}
+
+	return res, nil
+}
+
+func (s *MiniIO[T]) Delete(ctx context.Context, key string) error {
+	err := s.client.RemoveObject(
+		ctx,
+		s.bucketName,
+		key,
+		minio.RemoveObjectOptions{},
+	)
+
+	if err != nil {
+		return fmt.Errorf("failed to remove s3 object: %w", err)
+	}
+
+	return nil
+}
